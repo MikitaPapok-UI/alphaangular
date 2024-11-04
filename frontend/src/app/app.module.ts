275746be52b0838b5191bbc475685e99671ec908
papokmikita <mikita.papok@itechart-group.com>
2024-11-04 08:44:33 +0400

    initial commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { RouterOutlet } from "@angular/router";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [RouterOutlet],
+})
+export class AppModule {}
---
+++ frontend/src/main.ts
@@ -0,0 +1,9 @@
+import {
+  bootstrapApplication,
+  platformBrowser,
+} from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { AppComponent } from "./app/app.component";
+import { AppModule } from "./app/app.module";
+
+platformBrowser().bootstrapModule(AppModule);
